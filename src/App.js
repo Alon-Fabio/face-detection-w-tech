5129efc3e59df257958265918c61e6020e1f029e
Alon-Fabio <alon.the.fabio@gmail.com>
2020-01-22 18:00:07 +0200

    Added the API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -27,14 +28,47 @@ const ParticlesParans = {
 }
 }
 
+const app = new Clarifai.App({
+  apiKey: '3634fc260367403ca7e2a34a0b974b91'
+ });
+
+
 function App() {
+
+  const [input, setinput] = useState();
+  const [submitInput, setsubmitInput] = useState(0);
+
+  const SetInputState = (newState) => {
+    setinput(newState.target.value);
+    console.log(input);
+  };
+
+  const SubmitPicUrl = () => {
+    console.log('Click');
+    setsubmitInput(submitInput+1);
+  }
+
+  useEffect(()=>{
+    if(submitInput>0) {
+      app.models.initModel({id: Clarifai.FACE_DETECT_MODEL})
+      .then(generalModel => {
+        return generalModel.predict(input);
+      })
+      .then(response => {
+        // var concepts = response['outputs'][0]['data']['concepts'];
+        console.log(response)
+      }).catch(err=>console.log("LOG ERR" + err));
+    }
+
+  },[submitInput])
+
   return (
     <div className="App">
       <Particles className='Particles' params={ParticlesParans}/>
       <Navigetion />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm GetPicUrl={(event)=>SetInputState(event)} SubmitUrl={SubmitPicUrl} />
       {/* 
       <FaceRecognition /> */}
     </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({GetPicUrl,SubmitUrl}) => {
     return (
         <div className='ImageLinkForm'>
             <p className='f3'>{"This will detect faces in your picture, Give it a try !"}</p>
             <div className='flexCenter'>
                 <div className='InputField pa4 br3 shadow-5 w-50'>
-                    <input className='f4 pa2 w-70 center' type="text"></input>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' >{"Detect"}</button>
+                    <input onChange={GetPicUrl} className='f4 pa2 w-70 center' type="text"></input>
+                    <button onClick={SubmitUrl} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' >{"Detect"}</button>
                 </div>
             </div>
         </div>
